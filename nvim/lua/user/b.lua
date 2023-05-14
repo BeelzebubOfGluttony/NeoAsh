5a336c653008e50c6d0673069d0db78c99af99ae
BeelzebubOfGluttony <113144240+BeelzebubOfGluttony@users.noreply.github.com>
2023-05-14 12:15:03 -0500

    Delete nvim directory

--- nvim/lua/user/b.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, b = pcall(require, "bufferline")
-if not status_ok then
-  return
-end
-
-b.setup{
-
-}
--- nvim/lua/user/plugin_config/fine.lua
+++
@@ -1,7 +0,0 @@
--- fine-cmdline
-vim.api.nvim_set_keymap('n', ':', '<cmd>FineCmdline<CR>', {noremap = true})
-vim.o.undofile = true
-require('fundo').setup{
-
-}
-
